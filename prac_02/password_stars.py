2d0cf930c76b51414f839354ba998d623b225a7c
TrepidShark <spacecoop11@gmail.com>
2022-09-23 11:24:25 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-minimum_length = 5
-password = input("Assign your password: ")
-while len(password) < minimum_length:
-    print("Invalid Password")
+def main():
+    minimum_length = 5
+    password = get_password(minimum_length)
+    print_password(password)
+
+
+def print_password(password):
+    print("*" * len(password))
+
+
+def get_password(minimum_length):
     password = input("Assign your password: ")
-print("*" * len(password))
+    while len(password) < minimum_length:
+        print("Invalid Password")
+        password = input("Assign your password: ")
+    return password
+
+
+main()
