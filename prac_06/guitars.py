5b7f350baf133aaedf818931e517624e05d6b1a9
TrepidShark <spacecoop11@gmail.com>
2022-11-07 16:17:03 +1000

    Add completed guitar program

---
+++ prac_06/guitars.py
@@ -0,0 +1,20 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = int(input("Cost: "))
+        print(f"{name} ({year})  : ${cost} added.")
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name: ")
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = "vintage" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
+
+
+main()
